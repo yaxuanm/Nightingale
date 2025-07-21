c72d643cd7e8a89cc9f154e58bc0ed9d15f794dd
Yaxuan Mao <t-yaxuanmao@microsoft.com>
2025-07-21 17:01:56 +0800

    remove venv folders from git tracking and update .gitignore
    Former-commit-id: 634053bd1bb9860eab27c269e1c03e2594ebb3a7
    Former-commit-id: 79a160d3d31594d7c06d9020c35344503acd2c9f

--- backend/venv_stableaudio/Lib/site-packages/gradio/templates/frontend/assets/clone-e6We1twy.js
+++
@@ -1,2 +0,0 @@
-import{b as r}from"./_baseUniq-BfLOVHUl.js";var e=4;function a(o){return r(o,e)}export{a as c};
-//# sourceMappingURL=clone-e6We1twy.js.map
--- backend/venv_stableaudio/Lib/site-packages/gradio/templates/frontend/assets/logDepthDeclaration-BLCR3Ghe.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index-DNNy2Cov.js";const r="logDepthDeclaration",t=`#ifdef LOGARITHMICDEPTH
-uniform logarithmicDepthConstant: f32;varying vFragmentDepth: f32;
-#endif
-`;e.IncludesShadersStoreWGSL[r]||(e.IncludesShadersStoreWGSL[r]=t);
-//# sourceMappingURL=logDepthDeclaration-BLCR3Ghe.js.map
